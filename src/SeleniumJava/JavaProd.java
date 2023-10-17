ddb5469eb633cdc763502ed14efaf252e176fb0c
Syveryn Kot <Syveryn Kot@10.0.1.71>
2023-10-16 21:27:50 -0400

    Second

---
+++ src/SeleniumJava/JavaProd.java
@@ -0,0 +1,14 @@
+package SeleniumJava;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaProd {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\JavaSelenium\\driver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.youtube.com/");
+	}
+
+}
